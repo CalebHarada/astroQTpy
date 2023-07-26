f1e7b78f2729558330668fcd6d603681e496baf3
Caleb Harada <calebharada@calebs-mbp-16in.lan>
2023-07-25 18:04:48 -0700

    update random quadtree tutorial

--- astroqtpy/__init__.py
+++ astroqtpy/__init__.py
@@ -4,8 +4,8 @@ __version__ = "0.0.1"
 ##     - chi2 mapping tutorial notebook
 ##     - release on GitHub / Zenodo
 ##     - create readthedocs website
+
 ##     - email Kristo
-##
 ##     - advanced tutorial on defining a custion class
 ##     - injection/recovery class
 
